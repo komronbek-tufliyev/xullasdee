2136b290eb32c2c7c3d56faeefecdba0f2ff88ce
Komronbek Tufliyev <komronbek773@gmail.com>
2023-09-09 13:05:00 +0500

    Completed admin dashboard😎🎈. Abetdga gooo�🍕🍽

---
+++ config/settings/__init__.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+SECRET_KEY = env.str('SECRET_KEY')
+ENVIRONMENT = env.str('ENVIRONMENT', default='development')
+DEBUG = env.bool('DEBUG', default=True)
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+if ENVIRONMENT == 'development':
+    from .development import *
+    INSTALLED_APPS += ['debug_toolbar']
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    
+elif ENVIRONMENT == 'production':
+    from .production import *
+else:
+    raise ValueError('Invalid environment name')
