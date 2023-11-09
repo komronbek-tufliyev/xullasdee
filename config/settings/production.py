92a6ff0a44d48a91e48e02f4a53edc3559765ca6
Komronbek Tufliyev <komronbek773@gmail.com>
2023-11-09 16:07:00 +0500

    ka:

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,11 +3,14 @@ from .base import env
 DATABASES = {
     # amazon s3 bucket
     "default": {
-        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        # configured for mysql database
+        "ENGINE": 'django.db.backends.mysql',
         "NAME": env.str("DB_NAME"),
         "USER": env.str("DB_USER"),
         "PASSWORD": env.str("DB_PASSWORD"),
         "HOST": "localhost",# env.str("DB_HOST")        
-        "PORT": 5432, # env.str("DB_PORT")
+        "PORT": 5432, # env.str("DB_PORT"),
+        "ATOMIC_REQUESTS": True,
+        
     }
 }
